6deaf233aa34f76282db421b68a79419a368ff1c
yang-ze-kang <603822317@qq.com>
2022-05-11 22:03:25 +0800

    update logs: the discarded keys of model

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,12 @@
 from .vit import vit_base_patch16_224
 
 def create_model(cfg):
+    model_discard_keys = []
     model = eval(cfg['model_name'])
     if 'kargs' not in model.__code__.co_varnames:
         for k in list(cfg.keys()):
             if k not in model.__code__.co_varnames:
+                model_discard_keys.append(k)
                 cfg.pop(k)
+    print(k)
     return model(**cfg)
